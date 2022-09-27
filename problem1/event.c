85874f59cbf8cb3cf5ec68607e9afdc683281522
ElJuanes09 <juanex210404@gmail.com>
2022-09-26 21:35:48 -0500

    1er ver

--- problem1/event.c
+++ problem1/event.c
@@ -2,12 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 
-Event *CreateEvent(char *name)
+Event CreateEvent(charname)
 {
-
+    Event evento=malloc(sizeof(Event));
+    name[15]=0;
+    strncpy(evento->eventName,name,strlen(name) +1);
+    evento->next=NULL;
+    return evento;
 }
 
-void DestroyEvent(Event *this)
+void DestroyEvent(Eventthis)
 {
-
+    free(this);
 }
